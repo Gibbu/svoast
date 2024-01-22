e3922f8211a9ce2c72e4f6a3dd941af704688159
Gibbu <Gibbu_@hotmail.com>
2024-01-22 17:57:49 +1100

    Maybe fixed #8.

---
+++ build.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+
+const editedPkg = {
+	...pkg,
+	exports: {
+		'.': {
+			svelte: './index.js'
+		},
+		'./package.json': './package.json',
+		'./Icon.svelte': './Icon.svelte',
+		'.': './index.js',
+		'./stores': './stores.js',
+		'./Toast.svelte': './Toast.svelte',
+		'./Toasts.svelte': './Toasts.svelte',
+		'./types': './types.js',
+		'./utils': './utils.js'
+	}
+};
+
+fs.writeFileSync('./package/package.json', JSON.stringify(editedPkg, null, 2));
+
+console.log('Updated package.json.');
