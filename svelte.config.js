d724c2b52f706ca0ac56a4eee2b75bb80cadca29
Gibbu <Gibbu_@hotmail.com>
2024-01-25 20:53:33 +1100

    Updated site deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
